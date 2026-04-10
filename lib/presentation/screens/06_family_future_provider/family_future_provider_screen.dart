1d327f68448b5079a99b24ceaa67d3f07adae81f
BaldoCampos <baldocamposg@gmail.com>
2026-04-09 17:30:11 -0700

    refactor: :truck: Cambio de nombre a archivo

--- lib/presentation/screens/06_family_future_provider/family_future_provider_screen.dart
+++ lib/presentation/screens/06_family_future_provider/family_future_provider_screen.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
 
-
 class FamilyFutureScreen extends StatelessWidget {
   const FamilyFutureScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Family Future Provider'),
-      ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
-      ),
+      appBar: AppBar(title: const Text('Family Future Provider')),
+      body: const Center(child: Text('Fernando Herrera')),
       floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.refresh ),
-        onPressed: () {  },
+        child: const Icon(Icons.refresh),
+        onPressed: () {},
       ),
     );
   }
