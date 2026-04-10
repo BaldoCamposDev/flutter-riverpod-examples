a45b01d6021e4ff1d7d6016ad228699964fe59c9
BaldoCampos <baldocamposg@gmail.com>
2026-04-09 17:01:39 -0700

    style: :art: Format code

--- lib/presentation/screens/04_todo/todo_screen.dart
+++ lib/presentation/screens/04_todo/todo_screen.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 
-
 class TodoScreen extends StatelessWidget {
   const TodoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('State Provider + Providers'),
-      ),
+      appBar: AppBar(title: const Text('State Provider + Providers')),
       body: const _TodoView(),
       floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.add ),
+        child: const Icon(Icons.add),
         onPressed: () {},
       ),
     );
   }
 }
-
 
 class _TodoView extends StatelessWidget {
   const _TodoView();
@@ -33,17 +29,15 @@ class _TodoView extends StatelessWidget {
         ),
 
         SegmentedButton(
-          segments: const[
+          segments: const [
             ButtonSegment(value: 'all', icon: Text('Todos')),
             ButtonSegment(value: 'completed', icon: Text('Invitados')),
             ButtonSegment(value: 'pending', icon: Text('No invitados')),
-          ], 
-          selected: const <String>{ 'all' },
-          onSelectionChanged: (value) {
-            
-          },
+          ],
+          selected: const <String>{'all'},
+          onSelectionChanged: (value) {},
         ),
-        const SizedBox( height: 5 ),
+        const SizedBox(height: 5),
 
         /// Listado de personas a invitar
         Expanded(
@@ -51,12 +45,12 @@ class _TodoView extends StatelessWidget {
             itemBuilder: (context, index) {
               return SwitchListTile(
                 title: const Text('Juan carlos'),
-                value: true, 
-                onChanged: ( value ) {}
+                value: true,
+                onChanged: (value) {},
               );
             },
           ),
-        )
+        ),
       ],
     );
   }
